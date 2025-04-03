b5dfacf40759c069aef1276e0117cd09fef51f95
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2025-04-04 02:43:13 +0700

    CRUD member razor

--- Asm03Solution/Program.cs
+++ Asm03Solution/Program.cs
@@ -1,8 +1,10 @@
-using Asm03Solution.Components;
+﻿using Asm03Solution.Components;
 using BusinessObject.Services;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using DataAccess.Repositories;
+using Microsoft.AspNetCore.ResponseCompression;
+using BusinessObject.Services.Instance;
 
 
 namespace Asm03Solution
@@ -19,6 +21,32 @@ namespace Asm03Solution
             builder.Services.AddDbContext<ECommerceContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            // Cấu hình SignalR với thời gian chờ cao hơn
+            builder.Services.AddSignalR(options =>
+            {
+                options.ClientTimeoutInterval = TimeSpan.FromSeconds(60);
+                options.KeepAliveInterval = TimeSpan.FromSeconds(30);
+            });
+            // Thêm nén phản hồi để cải thiện hiệu suất
+            builder.Services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
+            // Enable Swagger/OpenAPI
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Cấu hình Cookie Policy
+            builder.Services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Register DbContext
+            builder.Services.AddDbContext<ECommerceContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             //cau hinh di repo+service
             builder.Services.AddScoped<MemberRepository>();
             builder.Services.AddScoped<MemberService>();
@@ -28,6 +56,10 @@ namespace Asm03Solution
             builder.Services.AddScoped<OrderDetailService>();
             builder.Services.AddScoped<ProductRepository>();
             builder.Services.AddScoped<ProductService>();
+            builder.Services.AddScoped<IMemberService, MemberService>();
+            // Register Repositories and Unit of Work
+            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             //cau hinh session
             builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
@@ -39,6 +71,23 @@ namespace Asm03Solution
                 options.Cookie.IsEssential = true;
             });
             var app = builder.Build();
+
+            // Initialize and seed the database
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    DbInitializer.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            // Sử dụng nén response
+            app.UseResponseCompression();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
